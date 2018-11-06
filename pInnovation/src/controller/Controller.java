327c9b5bfc70809e6cf7a994acf7b6f9a71833c6
Gautier <Gautier@DESKTOP-AEG8LF2>
2018-11-06 13:41:30 +0100

    Creation of all controllers

---
+++ pInnovation/src/controller/Controller.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class Controller {
+
+	private ControllerDB ctrlDB = new ControllerDB();
+	private ControllerDataView ctrlData = new ControllerDataView();
+	private ControllerView ctrlView = new ControllerView();
+	
+	
+	public Controller() {
+		
+	}
+	
+}
