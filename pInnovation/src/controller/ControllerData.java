318609a576ffde2520551dce565f1dc56a1e5496
Gautier <Gautier@DESKTOP-AEG8LF2>
2018-11-06 15:33:15 +0100

    ud

--- pInnovation/src/controller/ControllerData.java
+++ pInnovation/src/controller/ControllerData.java
@@ -1,5 +1,13 @@
 package controller;
 
 public class ControllerData {
+	
+	private ControllerDB ctrlDB;
+	private ControllerLink ctrlLink;
 
+	
+	public ControllerData () {
+		ctrlDB = new ControllerDB();
+		ctrlLink = new ControllerLink();
+	}
 }
