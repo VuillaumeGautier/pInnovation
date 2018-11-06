079cdf6046f0502ca9595db5a4951ee4fce8bbea
Gautier <Gautier@DESKTOP-AEG8LF2>
2018-11-06 13:41:45 +0100

    main update

--- pInnovation/src/main/Main.java
+++ pInnovation/src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import controller.Controller;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("TEST");
+		Controller controller = new Controller();
 
 	}
 
