dcc3ecf1c60473c701413f09512ba8b2336577ee
Gautier <Gautier@DESKTOP-AEG8LF2>
2018-11-06 13:59:50 +0100

    ud

--- pInnovation/src/controller/Controller.java
+++ pInnovation/src/controller/Controller.java
@@ -2,13 +2,22 @@ package controller;
 
 public class Controller {
 
-	private ControllerDB ctrlDB = new ControllerDB();
-	private ControllerDataView ctrlData = new ControllerDataView();
-	private ControllerView ctrlView = new ControllerView();
+	private ControllerDB ctrlDB;
+	private ControllerDataView ctrlData;
+	private ControllerView ctrlView;
 	
 	
 	public Controller() {
+		ctrlDB = new ControllerDB();
+		ctrlData = new ControllerDataView();
+		ctrlView = new ControllerView();
+	}
+	
+	public int main() {
 		
+		
+		
+		return 0;
 	}
 	
 }
