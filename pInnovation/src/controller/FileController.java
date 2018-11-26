bc565d05aaf66ce617c3bafba942e0ca829cd896
Gautier <Gautier@DESKTOP-AEG8LF2>
2018-11-26 11:22:27 +0100

    ddd

--- pInnovation/src/controller/FileController.java
+++ pInnovation/src/controller/FileController.java
@@ -3,9 +3,9 @@ package controller;
 public class FileController {
 	
 	// The name of the file to open.
-    String fileName = "temp.txt";
+    private String fileName = "temp.txt";
 
     // This will reference one line at a time
-    String line = null;
+    private String line = null;
 
 }
